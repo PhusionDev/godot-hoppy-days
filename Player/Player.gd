23b2d7dca9e7ce3088ca8335746c1d70b871c588
Jared Hodgkins <PhusionDev@gmail.com>
2021-01-04 08:28:12 -0500

    movement ease in/out

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,25 +2,23 @@ extends KinematicBody2D
 
 var motion = Vector2(0,0)
 const SPEED = 500
-const GRAVITY = 300
+const GRAVITY = 120
 const UP = Vector2(0,-1)
-const JUMP_SPEED = 3000
+const JUMP_FORCE = 2000
 
 func _physics_process(delta):
 	apply_gravity()
-	jump()
 	move()
-	move_and_slide(motion, UP)
+	animate()
+	jump()
+	motion = move_and_slide(motion, UP)
 
 func apply_gravity():
-	if not is_on_floor():
-		motion.y += GRAVITY
-	else:
-		motion.y = 0
-
+	motion.y += GRAVITY
+	
 func jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		motion.y -= JUMP_SPEED
+		motion.y = -JUMP_FORCE
 
 func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
@@ -29,3 +27,12 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0
+
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("jump")
+	elif not motion.x == 0:
+		$AnimatedSprite.flip_h = motion.x < 0
+		$AnimatedSprite.play("walk")
+	else:
+		$AnimatedSprite.play("idle")
